14e5a3dba70fe89e82ef479053031791c05f6efd
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-13 22:21:57 -0700

    Add api_base URL

--- sendbird/__init__.py
+++ sendbird/__init__.py
@@ -2,4 +2,7 @@
 from sendbird.api_resources import *  # NOQA
 
 api_key = None
-api_base = ""
+api_application_id = None
+api_base = "http://api-{application_id}.sendbird.com/v3/".format(
+	application_id=api_application_id
+)
