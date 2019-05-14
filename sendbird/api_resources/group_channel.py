b4a4186489b9aa5c489d0a3b3c856e91ea8a7576
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-13 22:24:33 -0700

    Add API resources for Group and Open Channels

---
+++ sendbird/api_resources/group_channel.py
@@ -0,0 +1,5 @@
+from sendbird.api_resources.channel import Channel
+
+
+class GroupChannel(Channnel):
+	pass
---
+++ sendbird/api_resources/open_channel.py
@@ -0,0 +1,5 @@
+from sendbird.api_resources.channel import Channel
+
+
+class OpenChannel(Channel):
+    pass
