dfe170a5a78f18320ca8500d8214c1713a1f3499
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-17 22:04:38 -0700

    Add User resource

--- sendbird/api_resources/__init__.py
+++ sendbird/api_resources/__init__.py
@@ -2,3 +2,4 @@ from sendbird.api_resources.channel import Channel  # NOQA
 from sendbird.api_resources.group_channel import GroupChannel  # NOQA
 from sendbird.api_resources.message import Message  # NOQA
 from sendbird.api_resources.open_channel import OpenChannel  # NOQA
+from sendbird.api_resources.user import User  # NOQA
