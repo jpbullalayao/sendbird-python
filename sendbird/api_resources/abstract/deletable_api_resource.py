8226d0faddfd8156e0b013e47e0686e8195e8818
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-17 22:08:05 -0700

    Fix typos, add DeleteableAPIResource

---
+++ sendbird/api_resources/abstract/deletable_api_resource.py
@@ -0,0 +1,6 @@
+from sendbird.api_resources.abstract.api_resource import APIResource
+
+
+class DeletableAPIResource(APIResource):
+	def delete(self):
+		pass
--- sendbird/api_resources/channel.py
+++ sendbird/api_resources/channel.py
@@ -1,10 +1,12 @@
 from sendbird.api_resources.abstract.createable_api_resource import CreateableAPIResource  # NOQA
+from sendbird.api_resources.abstract.deletable_api_resource import DeletableAPIResource  # NOQA
 from sendbird.api_resources.abstract.listable_api_resource import ListableAPIResource  # NOQA
-from sendbird.api_resources.abstract.listable_api_resource import UpdateableAPIResource  # NOQA
+from sendbird.api_resources.abstract.updateable_api_resource import UpdateableAPIResource  # NOQA
 
 
 class Channel(
 	CreateableAPIResource,
+	DeletableAPIResource,
 	ListableAPIResource,
 	UpdateableAPIResource
 ):
