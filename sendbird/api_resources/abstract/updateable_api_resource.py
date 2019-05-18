94ab2d4982d5b6b530e8202d2da98aaa01d1fb14
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-17 22:00:12 -0700

    Add UpdateableAPIResource

---
+++ sendbird/api_resources/abstract/updateable_api_resource.py
@@ -0,0 +1,5 @@
+from sendbird.api_resources.abstract.api_resource import APIResource
+
+
+class UpdateableAPIResource(APIResource):
+	pass
--- sendbird/api_resources/channel.py
+++ sendbird/api_resources/channel.py
@@ -1,9 +1,11 @@
 from sendbird.api_resources.abstract.createable_api_resource import CreateableAPIResource  # NOQA
 from sendbird.api_resources.abstract.listable_api_resource import ListableAPIResource  # NOQA
+from sendbird.api_resources.abstract.listable_api_resource import UpdateableAPIResource  # NOQA
 
 
 class Channel(
 	CreateableAPIResource,
 	ListableAPIResource,
+	UpdateableAPIResource
 ):
     pass
