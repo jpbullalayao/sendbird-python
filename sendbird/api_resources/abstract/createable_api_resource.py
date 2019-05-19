5ea1cb4ddabd321ac515de8f3830e1523c3900c2
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-19 12:25:29 -0700

    Convert CreateableAPIResource response to sendbird object

--- sendbird/api_resources/abstract/createable_api_resource.py
+++ sendbird/api_resources/abstract/createable_api_resource.py
@@ -1,6 +1,7 @@
 from sendbird.api_requestor import APIRequestor
 from sendbird.api_resources.abstract.api_resource import APIResource
 from sendbird.http_methods import HTTP_METHOD_POST
+from sendbird.util import convert_to_sendbird_object
 
 
 class CreateableAPIResource(APIResource):
@@ -15,4 +16,6 @@ class CreateableAPIResource(APIResource):
         )
 
         url = cls.class_url()
-        return requestor.request(HTTP_METHOD_POST, url, params)
+        response = requestor.request(HTTP_METHOD_POST, url, params)
+        sendbird_object = convert_to_sendbird_object(response)
+        return sendbird_object
