e74a9b70f2b275e9ef3b405b52b63748598629ca
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-18 23:41:40 -0700

    Allow ability to set attributes on dict

--- sendbird/sendbird_object.py
+++ sendbird/sendbird_object.py
@@ -9,11 +9,17 @@ class SendbirdObject(dict):
             pk_field = self.get_pk_field()
             self[pk_field] = pk
 
+    def __setattr__(self, k, v):
+        self[k] = v
+        return None
+
+    def __getattr__(self, k):
+        return self[k]
+
     @classmethod
     def get_pk_field(cls):
         return cls.PK_FIELD
 
     def refresh_from(self, values):
         for k, v in values.iteritems():
-            self[k] = v
-        
+            super(SendbirdObject, self).__setitem__(k, v)
