226b6ca36052229720b9e4e328e6afd3f5e64e0b
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-20 18:31:25 -0700

    Add Leave channel endpoint

--- sendbird/api_resources/group_channel.py
+++ sendbird/api_resources/group_channel.py
@@ -10,21 +10,22 @@ class GroupChannel(Channel):
         return self.request(http_methods.HTTP_METHOD_GET, url)
 
     def check_if_member(self, **params):
-    	if not params.get('user_id'):
-    		# TODO: Handle exception here
-    		pass
-
-    	url = self.instance_url() + api_endpoints.GROUP_CHANNEL_CHECK_IF_MEMBER
-    	return self.request(http_methods.HTTP_METHOD_GET, url, params=params)
+        url = self.instance_url() + api_endpoints.GROUP_CHANNEL_CHECK_IF_MEMBER
+        return self.request(http_methods.HTTP_METHOD_GET, url, params=params)
 
     def accept_invitation(self, **params):
-    	url = self.instance_url() + api_endpoints.GROUP_CHANNEL_ACCEPT_INVITATION
-    	return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
+        url = self.instance_url() + api_endpoints.GROUP_CHANNEL_ACCEPT_INVITATION
+        return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
 
     def reject_invitation(self, **params):
-    	url = self.instance_url() + api_endpoints.GROUP_CHANNEL_REJECT_INVITATION
-    	return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
+        url = self.instance_url() + api_endpoints.GROUP_CHANNEL_REJECT_INVITATION
+        return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
 
     def join(self, **params):
-    	url = self.instance_url() + api_endpoints.GROUP_CHANNEL_JOIN
-    	return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
+        url = self.instance_url() + api_endpoints.GROUP_CHANNEL_JOIN
+        return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
+
+    def leave(self, **params):
+        url = self.instance_url() + api_endpoints.GROUP_CHANNEL_LEAVE
+        return self.request(http_methods.HTTP_METHOD_PUT, url, params=params)
+
