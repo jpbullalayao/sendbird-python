f182e98757a3c22a24b47c3f466ad4adda0989a5
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-30 18:50:04 -0700

    Import all constants from channel file

--- sendbird/api_endpoints/__init__.py
+++ sendbird/api_endpoints/__init__.py
@@ -1,3 +1,10 @@
+from sendbird.api_endpoints.channel import CHANNEL_BAN_USER  # NOQA
+from sendbird.api_endpoints.channel import CHANNEL_FREEZE  # NOQA
+from sendbird.api_endpoints.channel import CHANNEL_LIST_BANNED_USERS  # NOQA
+from sendbird.api_endpoints.channel import CHANNEL_UNBAN_USER  # NOQA
+from sendbird.api_endpoints.channel import CHANNEL_UPDATE_BAN  # NOQA
+from sendbird.api_endpoints.channel import CHANNEL_VIEW_BAN  # NOQA
+
 from sendbird.api_endpoints.group_channel import GROUP_CHANNEL_ACCEPT_INVITATION  # NOQA
 from sendbird.api_endpoints.group_channel import GROUP_CHANNEL_CHECK_IF_MEMBER  # NOQA
 from sendbird.api_endpoints.group_channel import GROUP_CHANNEL_JOIN
