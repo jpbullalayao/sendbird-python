e3b6f5df27962d26b44c80074cf895ecf75610db
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2019-05-30 22:38:46 -0700

    Alphabetize endpoints in Channel

--- sendbird/api_endpoints/channel.py
+++ sendbird/api_endpoints/channel.py
@@ -1,8 +1,8 @@
 # All endpoint constants that both the OpenChannel and GroupChannel
 # resources share
 CHANNEL_BAN_USER = "/ban"
+CHANNEL_FREEZE = "/freeze"
 CHANNEL_LIST_BANNED_USERS = "/ban"
 CHANNEL_UNBAN_USER = "/ban/{banned_user_id}"
 CHANNEL_UPDATE_BAN = "/ban/{banned_user_id}"
 CHANNEL_VIEW_BAN = "/ban/{banned_user_id}"
-CHANNEL_FREEZE = "/freeze"
